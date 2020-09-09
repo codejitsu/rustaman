8ddb6b92be0d8b4f4a5ca31cfb5bf0a87ecbde54
codejitsu <me@codejitsu.net>
2020-09-10 01:10:07 +0200

    Add filtering all folders which contain '.git' subdirectory.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    for entry in WalkDir::new(".")
+            .follow_links(true)
+            .into_iter()
+            .filter_map(|e| e.ok()) {
+        let f_name = entry.file_name().to_string_lossy();
+
+        if f_name == ".git" {
+            println!("{}", entry.path().parent().unwrap().display());
+        }
+    }
 }
