a7f2dce60a4a0298a4725bbb44afba57eb828fae
codejitsu <me@codejitsu.net>
2020-09-10 18:47:08 +0200

    Extract and display branch name for git repository

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
+use std::path::Path;
+use std::fs;
+use termion::color;
+
+fn extract_branch(entry: &DirEntry) -> Option<String> {
+    let head_path = format!("{}/HEAD", entry.path().to_string_lossy());
+
+    log::debug!("Checking HEAD for {}", head_path);
+
+    if !Path::new(&head_path).exists() {
+        log::debug!("{} does not exist", head_path);
+        None
+    } else {
+        log::debug!("{} does exist", head_path);
+        log::debug!("{}", entry.path().to_string_lossy());
+
+        let branch_line = fs::read_to_string(head_path).expect("Something went wrong reading the file");
+
+        let clean_branch = branch_line.trim_end_matches(&['\r', '\n'][..]);
+        let branch_parts = clean_branch.split("/");
+        let branch_vector: Vec<&str> = branch_parts.collect();
+        let branch_name = branch_vector.last().map_or("", |b| b);
+
+        Some(String::from(branch_name))
+    }
+}
 
 fn main() {
+    env_logger::init();
+
     for entry in WalkDir::new(".")
             .follow_links(true)
             .into_iter()
@@ -8,7 +36,19 @@ fn main() {
         let f_name = entry.file_name().to_string_lossy();
 
         if f_name == ".git" {
-            println!("{}", entry.path().parent().unwrap().display());
+            let msg = extract_branch(&entry)
+                .map(|branch| (entry.path().parent().unwrap().display().to_string(), branch))
+                .unwrap_or((String::from(""), String::from("")));
+
+            if msg != (String::from(""), String::from("")) {    
+                print!("{}", color::Fg(color::Green));
+                print!("{}", msg.0);
+                print!("{}", color::Fg(color::Cyan));
+                print!("{}", " -> ");
+                print!("{}", color::Fg(color::Yellow));
+                print!("{}", msg.1);
+                print!("{}", "\n");
+            }
         }
     }
 }
